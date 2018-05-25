c3ff82c156f6522470b068452b05f693ad640838
Sokha <cantorethomas@gmail.com>
2018-05-25 19:43:36 +0200

    Show function upgraded

--- R/useful_methods.R
+++ R/useful_methods.R
@@ -8,23 +8,54 @@ NULL
 setMethod("show", "ScudoResults", function(object) {
     cat("Object of ScudoResults\n\n")
 
-    cat("Distance Matrix\n")
-    print(object@DistMatrix[1:5,1:5])
+    d1 <- dim(object@DistMatrix)
+    cat("Distance Matrix \ttot dim:", d1[1], "rows and ", d1[2], "cols\n")
+    if (all(d1 > 5)) {
+        cat("First 5 rows and cols\n")
+        print(object@DistMatrix[1:5,1:5])
+    } else {
+        cat("First ", dim(object@DistMatrix)[1], "rows and cols\n")
+        print(object@DistMatrix)
+    }
     cat("\n")
 
-    cat("Up Signatures\n")
-    print(object@UpSignatures[1:5,1:5])
+    d2 <- dim(object@UpSignatures)
+    cat("Up Signatures \t\ttot dim:", d2[1], "rows and ", d2[2], "cols\n")
+    if (all(d2 > 5)) {
+        cat("First 5 rows and cols\n")
+        print(object@UpSignatures[1:5,1:5])
+    } else {
+        d2_min <- min(d2)
+        cat("First ", d2_min, "rows and cols\n")
+        print(object@UpSignatures[1:d2_min,1:d2_min])
+    }
     cat("\n")
 
-    cat("Down Signatures\n")
-    print(object@DownSignatures[1:5,1:5])
+    d3 <- dim(object@DownSignatures)
+    cat("Down Signatures \ttot dim:", d3[1], "rows and ", d3[2], "cols\n")
+    if (all(d3 > 5)) {
+        cat("First 5 rows and cols\n")
+        print(DownSignatures(object)[1:5,1:5])
+        cat("\t...\n", object@DownSignatures[-1,])
+    } else {
+        d3_min <- min(d3)
+        cat("First ", d3_min, "rows and cols\n")
+        print(object@DownSignatures[1:d3_min,1:d3_min])
+    }
     cat("\n")
 
-    cat("Selected Features\n", object@SelectedFeatures,
-        "\n\n")
+    d4 <- length(object@SelectedFeatures)
+    cat("Selected Features \ttot dim:", d4, "\n")
+    if (all(d4 > 10)) {
+        cat("First 10: ")
+        print(ScudoRes@SelectedFeatures[1:10])
+    } else {
+       print(object@SelectedFeatures)
+    }
 
     invisible(NULL)
 })
+
 
 # Summary ---------------------------------------------------------------------
 
