1700fbec4d4a24395f28567d2f4d6d8c7d869cb1
Enrica Colasurdo <enrica.colasurdo@gmail.com>
2018-05-25 13:10:00 +0200

    summary method (untested)

--- R/useful_methods.R
+++ R/useful_methods.R
@@ -4,4 +4,18 @@ NULL
 
 # show
 # summary
+#ADD n NUMBER OF SAMPLES
+setMethod("summary", signature = "ScudoResults", definition =
+              function(object) {
+                  cat("The analysis is performed on", n, "samples",
+                      "\nThe upper signatures are made of",
+                        dim(UpSignatures(object))[1], "genes",
+                      "\nThe upper signatures are made of",
+                        dim(DownSignatures(object))[1], "genes",
+                      "\nThe consensus sequence is:\n\t",
+                        c(ConsensusUpSignature(object),
+                          ConsensusDownSignature(object)))})
+
+
 # ConsensusSignatures: returns a list of ConsensusUp.. and Down
+
