41dcb93fd709366b57a0217468d20e75e5333bb2
Sokha <cantorethomas@gmail.com>
2018-06-01 21:56:00 +0200

    Checks and tests for scudo function

--- R/accessors.R
+++ R/accessors.R
@@ -14,47 +14,89 @@ setGeneric("DistMatrix", function(object) standardGeneric("DistMatrix"))
 setMethod("DistMatrix", signature = "ScudoResults", definition =
               function(object) object@DistMatrix)
 
+#' @rdname ScudoResults-class
+#' @aliases UpSignatures,ScudoResults-method
+#' @usage NULL
 #' @export
 setGeneric("UpSignatures", function(object) standardGeneric("UpSignatures"))
 
+#' @rdname ScudoResults-class
+#' @aliases UpSignatures,ScudoResults-method
+#' @usage NULL
 setMethod("UpSignatures", signature = "ScudoResults", definition =
               function(object) object@UpSignatures)
 
+#' @rdname ScudoResults-class
+#' @aliases DownSignatures,ScudoResults-method
+#' @usage NULL
 #' @export
 setGeneric("DownSignatures", function(object) standardGeneric("DownSignatures"))
 
+#' @rdname ScudoResults-class
+#' @aliases DownSignatures,ScudoResults-method
+#' @usage NULL
 setMethod("DownSignatures", signature = "ScudoResults", definition =
               function(object) object@DownSignatures)
 
+#' @rdname ScudoResults-class
+#' @aliases Groups,ScudoResults-method
+#' @usage NULL
 #' @export
 setGeneric("Groups", function(object) standardGeneric("Groups"))
 
+#' @rdname ScudoResults-class
+#' @aliases Groups,ScudoResults-method
+#' @usage NULL
 setMethod("Groups", signature = "ScudoResults", definition =
               function(object) object@Groups)
 
+#' @rdname ScudoResults-class
+#' @aliases ConsensusUpSignatures,ScudoResults-method
+#' @usage NULL
 #' @export
 setGeneric("ConsensusUpSignatures",
            function(object) standardGeneric("ConsensusUpSignatures"))
 
+#' @rdname ScudoResults-class
+#' @aliases Groups,ScudoResults-method
+#' @usage NULL
 setMethod("ConsensusUpSignatures", signature = "ScudoResults", definition =
               function(object) object@ConsensusUpSignatures)
 
+#' @rdname ScudoResults-class
+#' @aliases ConsensusDownSignatures,ScudoResults-method
+#' @usage NULL
 #' @export
 setGeneric("ConsensusDownSignatures",
            function(object) standardGeneric("ConsensusDownSignatures"))
 
+#' @rdname ScudoResults-class
+#' @aliases ConsensusDownSignatures,ScudoResults-method
+#' @usage NULL
 setMethod("ConsensusDownSignatures", signature = "ScudoResults", definition =
               function(object) object@ConsensusDownSignatures)
 
+#' @rdname ScudoResults-class
+#' @aliases SelectedFeatures,ScudoResults-method
+#' @usage NULL
 #' @export
 setGeneric("SelectedFeatures",
            function(object) standardGeneric("SelectedFeatures"))
 
+#' @rdname ScudoResults-class
+#' @aliases SelectedFeatures,ScudoResults-method
+#' @usage NULL
 setMethod("SelectedFeatures", signature = "ScudoResults", definition =
               function(object) object@SelectedFeatures)
 
+#' @rdname ScudoResults-class
+#' @aliases Params,ScudoResults-method
+#' @usage NULL
 #' @export
 setGeneric("Params", function(object) standardGeneric("Params"))
 
+#' @rdname ScudoResults-class
+#' @aliases Params,ScudoResults-method
+#' @usage NULL
 setMethod("Params", signature = "ScudoResults", definition =
               function(object) object@Params)
--- tests/testthat/test_scudo.R
+++ tests/testthat/test_scudo.R
@@ -1,15 +1,84 @@
-context("Test scudo preprocessing")
+context("Test scudo")
 library(scudo)
 
-test_that("Feature selection works", {
+test_that("Raises errors on wrong inputs", {
 
-    groups2 <- c(rep("A", 50), rep("B", 50))
-    ExprMatrix <- matrix(1, ncol = 100, nrow = 100)
-    ExprMatrix <- apply(ExprMatrix, 1, function(x) x <- stats::runif(100,0,100))
-    rownames(ExprMatrix) <- c(rep("k", 25), c(rep("x", 25)),
-        c(rep("y", 25)), c(rep("z", 25)))
-    colnames(ExprMatrix) <- groups2
+    exData <- matrix(1, ncol = 4, nrow = 4)
+    rownames(exData) <- letters[1:4]
+    colnames(exData) <- letters[5:8]
 
-    expect_equal(2,2)
+    gr1 <- factor(c("h","h","d","d"))
+    gr2 <- c("h", "h", "d", "d")
+    gr3 <- factor(c("h","d","hd","h"))
+    gr4 <- factor(rep("h",4))
 
+    # Tests on expressionData errors ------------------------------------------
+
+    exDatadf <- as.data.frame(exData)
+    expect_error(scudo(exDatadf, gr1, 1, 3, 0.1))
+
+    exData[,1] <- NA
+    expect_error(scudo(exData, gr1, 1, 3, 0.1))
+
+    exData[,1] <- NaN
+    expect_error(scudo(exData, gr1, 1, 3, 0.1))
+
+    exData[,1] <- letters[1:4]
+    expect_error(scudo(exData, gr1, 1, 3, 0.1))
+
+    exData <- matrix(1, ncol = 4, nrow = 4)
+    colnames(exData) <- letters[5:8]
+    rownames(exData) <- NULL
+    expect_error(scudo(exData, gr1, 1, 3, 0.1))
+
+    rownames(exData) <- letters[1:4]
+    colnames(exData) <- NULL
+    expect_error(scudo(exData, gr1, 1, 3, 0.1))
+
+    colnames(exData) <- letters[5:8]
+
+    # Test on group errors ----------------------------------------------------
+
+    expect_error(scudo(exData, gr2, 1, 3, 0.1))
+
+    expect_error(scudo(exData, gr3, 1, 3, 0.1))
+
+    #expect_warning(scudo(exData, gr4, 1, 3, 0.1))
+
+    # Test on nTop, nBottom errors --------------------------------------------
+
+    expect_error(scudo(exData, gr1, "a", 3, 0.1))
+
+    expect_error(scudo(exData, gr1, 1, "a", 0.1))
+
+    expect_error(scudo(exData, gr1, NaN, 3, 0.1))
+
+    expect_error(scudo(exData, gr1, 1, NaN, 0.1))
+
+    #expect_warning(scudo(exData, gr1, 0, 0, 0.1))
+
+    expect_error(scudo(exData, gr1, -1, 3, 0.1))
+
+    expect_error(scudo(exData, gr1, 0.1, 3, 0.1))
+
+    # Test pValue, prepro, featureSel and p.adj -------------------------------
+
+    expect_error(scudo(exData, gr1, 1, 3, "a"))
+
+    expect_error(scudo(exData, gr1, 1, 3, NaN))
+
+    expect_error(scudo(exData, gr1, 1, 3, -1))
+
+    expect_error(scudo(exData, gr1, 1, 3, 1.2))
+
+    expect_error(scudo(exData, gr1, 1, 3, 0.1, prepro = "ABC"))
+
+    expect_error(scudo(exData, gr1, 1, 3, 0.1, featureSel = "ABC"))
+
+    expect_error(scudo(exData, gr1, 1, 3, 0.1, p.adj = "ABC"))
+
+    expect_error(scudo(exData, gr1, 1, 4, 0.1))
 })
+
+# expect_warning goes in conflict with .Normalization that raises errors:
+# check commented tests
