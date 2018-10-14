a0af948fd6fd3c1c41b47cee7287fb97d7db4376
Matteo Ciciani <matteo.ciciani@gmail.com>
2018-10-14 17:10:22 +0200

    Modified normalization

--- R/utilities.R
+++ R/utilities.R
@@ -198,7 +198,7 @@ NULL
     }
 
     qx <- as.numeric(stats::quantile(ExpressionData,
-                              c(0., 0.25, 0.5, 0.75, 0.99, 1.0), na.rm=T))
+        c(0., 0.25, 0.5, 0.75, 0.99, 1.0), na.rm=T))
     logC <- (qx[5] > 100) || #checking if not log transfromed
         (qx[6]-qx[1] > 50 && qx[2] > 0) ||
         (qx[2] > 0 && qx[2] < 1 && qx[4] > 1 && qx[4] < 2)
