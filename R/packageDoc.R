5e35db40aecbfa17030041ced918e45f9ba893a5
Matteo Ciciani <matteo.ciciani@gmail.com>
2018-10-25 12:14:52 +0200

    Added skeleton of package Rd file

---
+++ R/packageDoc.R
@@ -0,0 +1,19 @@
+#' test1
+#'
+#' test2
+#'
+#' test3
+#'
+#' @seealso \code{\link{scudoTrain}}, \code{\link{scudoNetwork}},
+#' \code{\link{scudoClassify}}
+#'
+#' @author Matteo Ciciani \email{matteo.ciciani@@gmail.com}, Thomas Cantore
+#' \email{cantorethomas@@gmail.com}
+#'
+#' @examples
+#' example("scudoTrain")
+#'
+#' @docType package
+#' @name rScudo-package
+#' @rdname rScudo-package
+NULL
