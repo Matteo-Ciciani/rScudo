27988a283f12a96f7b8c4da50a06bbe5b31b23e8
Matteo Ciciani <matteo.ciciani@gmail.com>
2018-10-29 23:11:31 +0100

    Updated package description

--- R/packageDoc.R
+++ R/packageDoc.R
@@ -1,13 +1,15 @@
 #' Signature-based Clustering for Diagnostic Purposes
 #'
-#' SCUDO is a rank-based method for the analysis of gene expression profiles for
-#' diagnostic and classification purposes. It is based on the identification of
+#' SCUDO (Signature-based Clustering for Diagnostic Purposes) is a rank-based
+#' method for the analysis of gene expression profiles for diagnostic and
+#' classification purposes. It is based on the identification of sample-specific
 #' gene signatures composed of up- and down-regulated genes. Starting from gene
 #' expression data, functions in this package identify sample-specific gene
 #' signatures and use them to build a graph of samples, that are joined by edges
-#' if they have a similar expression profile. The graph can then be used to
-#' perform community clustering or to perform supervised classification of
-#' samples in a testing set.
+#' if they have a similar expression profile. The similarity between the
+#' expression profile of two sample is computed using a method similat to GSEA.
+#' The graph of samples can the be used to perform community clustering or to
+#' perform supervised classification of samples in a testing set.
 #'
 #' @seealso \code{\link{scudoTrain}}, \code{\link{scudoNetwork}},
 #' \code{\link{scudoClassify}}
@@ -16,7 +18,8 @@
 #' \email{cantorethomas@@gmail.com}
 #'
 #' @examples
-#' example("scudoTrain")
+#' # To learn more about rScudo, start with the vignette:
+#' browseVignettes("rScudo")
 #'
 #' @docType package
 #' @name rScudo-package
